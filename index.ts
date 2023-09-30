85115f0f6ce5eb1354a47858d2eccf0828d7960f
Alki Ooms <alki.ooms@student.ap.be>
2023-09-30 23:55:30 +0200

    Setup empty express app + ejs views

---
+++ index.ts
@@ -0,0 +1,64 @@
+import express from "express";
+
+const app = express();
+
+app.set("port", 3000);
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(express.json({ limit: "1mb" }));
+app.use(express.urlencoded({ extended: true }));
+
+
+app.get("/", (req, res) => {
+    // e.g. http://localhost:3000/
+    res.render("index");
+})
+
+app.get("/login", (req, res) => {
+    // e.g. http://localhost:3000/login
+    res.render("login");
+})
+
+app.get("/register", (req, res) => {
+    // e.g. http://localhost:3000/register
+    res.render("register");
+})
+
+app.get("/user/:userId/quiz", (req, res) => {
+    // e.g. http://localhost:3000/user/1/quiz
+    res.render("quiz");
+})
+
+app.get("/user/:userId/quiz/:type/question/:questionId", (req, res) => {
+    // e.g. http://localhost:3000/user/1/quiz/10rounds/question/1
+    res.render("question");
+})
+
+app.get("/user/:userId/quiz/:type/score", (req, res) => {
+    // e.g. http://localhost:3000/user/1/quiz/10rounds/score
+    res.render("score");
+})
+
+app.get("/user/:userId/favorites", (req, res) => {
+    // e.g. http://localhost:3000/user/1/favorites
+    res.render("favorites");
+})
+
+app.get("/user/:userId/favorites/:characterId", (req, res) => {
+    // e.g. http://localhost:3000/user/1/favorites/28392
+    res.render("character");
+})
+
+app.get("/user/:userId/blacklist", (req, res) => {
+    // e.g. http://localhost:3000/user/1/blacklist
+    res.render("blacklist");
+})
+
+app.use((req, res) => {
+    res.status(404);
+    res.send("Error 404 - Page Not Found");
+})
+
+app.listen(app.get("port"), () => {
+    console.log(`Local url: htpp://localhost:${app.get("port")}`)
+})
