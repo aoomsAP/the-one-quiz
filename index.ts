78db803cd6f773e3fa939a537168c41b73f80fc6
Chiani Egels <chiani.egels@student.ap.be>
2023-10-05 18:36:31 +0200

    input fields created

--- index.ts
+++ index.ts
@@ -9,52 +9,52 @@ app.use(express.json({ limit: "1mb" }));
 app.use(express.urlencoded({ extended: true }));
 
 
-app.get("/", (req, res) => {
+app.get("/", (req: any, res: any) => {
     // e.g. http://localhost:3000/
     res.render("index");
 })
 
-app.get("/login", (req, res) => {
+app.get("/login", (req: any, res: any) => {
     // e.g. http://localhost:3000/login
     res.render("login");
 })
 
-app.get("/register", (req, res) => {
+app.get("/register", (req: any, res: any) => {
     // e.g. http://localhost:3000/register
     res.render("register");
 })
 
-app.get("/user/:userId/quiz", (req, res) => {
+app.get("/user/:userId/quiz", (req: any, res: any) => {
     // e.g. http://localhost:3000/user/1/quiz
     res.render("quiz");
 })
 
-app.get("/user/:userId/quiz/:type/question/:questionId", (req, res) => {
+app.get("/user/:userId/quiz/:type/question/:questionId", (req: any, res: any) => {
     // e.g. http://localhost:3000/user/1/quiz/10rounds/question/1
     res.render("question");
 })
 
-app.get("/user/:userId/quiz/:type/score", (req, res) => {
+app.get("/user/:userId/quiz/:type/score", (req: any, res: any) => {
     // e.g. http://localhost:3000/user/1/quiz/10rounds/score
     res.render("score");
 })
 
-app.get("/user/:userId/favorites", (req, res) => {
+app.get("/user/:userId/favorites", (req: any, res: any) => {
     // e.g. http://localhost:3000/user/1/favorites
     res.render("favorites");
 })
 
-app.get("/user/:userId/favorites/:characterId", (req, res) => {
+app.get("/user/:userId/favorites/:characterId", (req: any, res: any) => {
     // e.g. http://localhost:3000/user/1/favorites/28392
     res.render("character");
 })
 
-app.get("/user/:userId/blacklist", (req, res) => {
+app.get("/user/:userId/blacklist", (req: any, res: any) => {
     // e.g. http://localhost:3000/user/1/blacklist
     res.render("blacklist");
 })
 
-app.use((req, res) => {
+app.use((req: any, res: any) => {
     res.status(404);
     res.send("Error 404 - Page Not Found");
 })
