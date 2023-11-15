4246bd70cadaab4dc3ff54834cb526ce59730796
Tatjana Torchenyuk <tatjana.torchenyuk@student.ap.be>
2023-11-15 22:42:17 +0100

    changed form to post in register.ejs, added createUser and getUser func in db.ts and implemented /register GET and POST routes

--- index.ts
+++ index.ts
@@ -1,8 +1,7 @@
 require("dotenv").config();
 import express from "express";
-import { connect } from "./db";
+import { connect, createUser, getUser } from "./db";
 import { User, Favorite, Blacklist, Question, Quote, Movie, Character } from "./types";
-import { ObjectId } from "mongodb";
 import { mockUser, mockQuotes, mockMovies, mockCharacters, mockQuestions } from "./mockData";
 
 const app = express();
@@ -33,12 +32,41 @@ app.get("/", (req, res) => {
 
 app.post("/login", (req, res) => {
     // e.g. http://localhost:3000/login
-    res.render("login");
+
 })
 
 app.get("/register", (req, res) => {
     // e.g. http://localhost:3000/register
+
     res.render("register");
+})
+
+app.post("/register", async (req, res) => {
+    // e.g. http://localhost:3000/register
+
+    let username: string = req.body.username;
+    let password: string = req.body.password;
+    let email: string = req.body.email;
+
+    let newUser: User = {
+        username: username,
+        password: password,
+        email: email,
+        favorites: [],
+        blacklist: [],
+        highscore_tenrounds: 0,
+        highscore_suddendeath: 0
+    }
+
+    let foundUser: User | null = await getUser(newUser);
+
+    if (foundUser) {
+        return res.status(409).json({ "error": "User already exists." });
+    }
+
+    await createUser(newUser);
+    user = newUser;
+    return res.status(201).redirect("/");
 })
 
 app.get("/user/:userId/quiz", (req, res) => {
@@ -254,3 +282,7 @@ app.listen(app.get("port"), async () => {
         console.log("Error: MongoDB connection failed.");
     }
 })
+
+function findUser(username: string) {
+    throw new Error("Function not implemented.");
+}
